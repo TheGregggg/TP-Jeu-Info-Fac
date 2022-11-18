fe16a531e99753124e24f560f209c2d7d9a175b4
Gregggg <38972165+TheGregggg@users.noreply.github.com>
2022-11-18 10:34:38 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,39 @@
+function RectanglePleinArondie(x, y, l, h, r, c) {
+  RectanglePlein(x, y + r, l, h - r * 2, c);
+  RectanglePlein(x + r, y, l - r * 2, h, c);
 
-
-
-function RectanglePleinArondie(x, y, l, h, r, c){
-	RectanglePlein(x  ,y+r,l  ,h-r*2,c);
-	RectanglePlein(x+r,y  ,l-r*2,h  ,c);
-  
-	CerclePlein(x+r  ,y+r  ,r*2, c);
-	CerclePlein(x+l-r,y+r  ,r*2, c);
-	CerclePlein(x+r  ,y+h-r,r*2, c);
-	CerclePlein(x+l-r,y+h-r,r*2, c);
+  CerclePlein(x + r, y + r, r * 2, c);
+  CerclePlein(x + l - r, y + r, r * 2, c);
+  CerclePlein(x + r, y + h - r, r * 2, c);
+  CerclePlein(x + l - r, y + h - r, r * 2, c);
 }
 
-function collide(x,y,l,h){
-  if((x <= mouseX) && (mouseX <= x+l) && (y <= mouseY) && (mouseY <= y+h) ){
+function collide(x, y, l, h) {
+  if ((x <= mouseX) && (mouseX <= x + l) && (y <= mouseY) && (mouseY <= y + h)) {
     return true;
-  }
-  else{
+  } else {
     return false;
   }
 }
 
 
-function mon_jeu(){
-  setInterval(function (){
-    RectanglePleinArondie(100,100,400,800,50, 'red');
-    
-    if(collide(100,100,400,800)){
-      RectanglePleinArondie(100,100,400,800,50, 'blue');
+var boucle_jeu = null;
+
+
+function mon_jeu() {
+    boucle_jeu = setInterval(function() {
+    RectanglePleinArondie(100, 100, 400, 800, 50, 'red');
+
+    if (collide(100, 100, 400, 800)) {
+      RectanglePleinArondie(100, 100, 400, 800, 50, 'blue');
     }
-   
-  },1000/60);
+
+    RectanglePleinArondie(1500, 800, 100, 100, 20, 'red');
+    if (collide(1500, 800, 100, 100)) {
+        clearInterval(boucle_jeu);
+    }
+
+  }, 1000 / 60);
 }
 
 mon_jeu();
